b0ddb7e5f843d988be92ea2ccc0eb1c6f80d8764
Lee Witek <neomech@gmail.com>
2016-05-23 23:02:41 +0100

    trying pagination

--- cmd/streak/main.go
+++ cmd/streak/main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "github.com/simulatedsimian/strk/streak"
 
+func main() {
+	streak.GetRepos("simulatedsimian")
 }
--- streak/repos.go
+++ streak/repos.go
@@ -1,6 +1,11 @@
 package streak
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
 
 type Repo struct {
 	Name        string
@@ -10,4 +15,31 @@ type Repo struct {
 
 func GetRepos(user string) ([]string, error) {
 
+	r, err := http.NewRequest("GET", "https://api.github.com/users/"+user+"/repos?page=1&per_page=10", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(r)
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range resp.Header {
+		fmt.Println(k, v)
+	}
+
+	defer resp.Body.Close()
+
+	var repos ReposJSON
+
+	err = json.NewDecoder(resp.Body).Decode(&repos)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(len(repos))
+
+	return nil, nil
 }
