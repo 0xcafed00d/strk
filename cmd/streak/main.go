a6e1cfcade30c62e84b859d86458b639b3f3895a
Lee Witek <neomech@gmail.com>
2016-05-27 14:44:40 +0100

    experimenting with multiple concurrent requests

--- cmd/streak/main.go
+++ cmd/streak/main.go
@@ -2,25 +2,58 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/simulatedsimian/strk/streak"
 )
 
+type repoCommits struct {
+	repoName string
+	*streak.CommitsJSON
+}
+
 func main() {
 
-	repos, err := streak.GetRepos("simulatedsimian", os.Args[1])
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	repoChan := make(chan string)
+	commitsChan := make(chan repoCommits)
 
-	for _, v := range *repos {
-		commits, err := streak.GetCommits("simulatedsimian", v.Name, os.Args[1])
+	go func() {
+		repos, err := streak.GetRepos("simulatedsimian", os.Args[1])
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		fmt.Println(v.Name, len(*commits))
+
+		for _, v := range *repos {
+			repoChan <- v.Name
+		}
+		close(repoChan)
+	}()
+
+	for n := 0; n < 16; n++ {
+		go func(n int) {
+			log.Println("Staring go func", n)
+			for {
+				name := <-repoChan
+				if name == "" {
+					break
+				}
+				log.Println("Fetching commits for repo: ", name)
+
+				commits, err := streak.GetCommits("simulatedsimian", name, os.Args[1])
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				commitsChan <- repoCommits{name, commits}
+			}
+			log.Println("End go func", n)
+		}(n)
+	}
+
+	for {
+		res := <-commitsChan
+		log.Println(res.repoName, len(*res.CommitsJSON))
 	}
 }
